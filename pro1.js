68390ae4f53cef506c6222a1444ae8002191ce5d
Rifat1000N <111520650+Rifat1000N@users.noreply.github.com>
2025-09-17 14:50:56 +0600

    Update pro1.js

--- pro1.js
+++ pro1.js
@@ -61,7 +61,7 @@ function addToCart() {
         name: 'ELARA Dress',
         price: 850,
         quantity: 1,
-        image: '/pic1.webp'
+        image: 'pro1/ppf.webp'
     };
 
     const existingItem = cart.find(item => item.id === newItem.id);
