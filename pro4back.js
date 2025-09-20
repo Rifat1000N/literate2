4b759e47f611e69527bde23e34043add0a4a5f50
Rifat1000N <111520650+Rifat1000N@users.noreply.github.com>
2025-09-20 15:01:51 +0600

    Update pro4back.js

--- pro4back.js
+++ pro4back.js
@@ -100,7 +100,7 @@ function addToCart() {
         name: 'ELARA Dress',
         price: 850,
         quantity: 1,
-        image: 'pro3/pp1.webp'
+        image: 'pro4/pp1.webp'
     };
 
     const existingItem = cart.find(item => item.id === newItem.id);
